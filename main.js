4eb971776c66dab0c5a6088e717f6604e29dab4b
Stepaha007 <banetuser1@mail.ru>
2019-07-14 05:08:52 +0300

    Add main.js README.md node_modules

--- main.js
+++ main.js
@@ -3,29 +3,30 @@ express = require('express'),
 cookieParser = require('cookie-parser'),
 bodyParser = require('body-parser'),
 path = require('path');
-
-var app = express();
+var app = express()
+server = require('http').createServer(app)
+  
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+//app.set('views', path.join(__dirname, '/views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get('/', (req, res)=>{
-  res.render('index.ejs')
+  res.render('/views/index.ejs')
 })
 
 app.get('/info', (req, res)=>{
-  res.render('info.ejs')
+  res.render('/views/info.ejs')
 })
 
 app.get('/tos', (req, res)=>{
-  res.render('tos.ejs')
+  res.render('/views/tos.ejs')
 })
 
 app.get('*', (req, res)=>{
-  res.render('404.ejs')
+  res.render('/views/404.ejs')
 })
 
-app.listen(80, () => console.log(`App running on port ${80}`));
+server.listen(80, () => console.log(`App running on port ${80}`));
