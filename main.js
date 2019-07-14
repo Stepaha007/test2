62cb461ac8f60510956f2d12b9edb4b8ac59fde3
Stepaha007 <banetuser1@mail.ru>
2019-07-14 03:17:57 +0300

    Add public node_modules views main.js package-lock.json

---
+++ main.js
@@ -0,0 +1,31 @@
+const
+express = require('express'),
+cookieParser = require('cookie-parser'),
+bodyParser = require('body-parser'),
+path = require('path');
+
+var app = express();
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', (req, res)=>{
+  res.render('index.ejs')
+})
+
+app.get('/info', (req, res)=>{
+  res.render('info.ejs')
+})
+
+app.get('/tos', (req, res)=>{
+  res.render('tos.ejs')
+})
+
+app.get('*', (req, res)=>{
+  res.render('404.ejs')
+})
+
+app.listen(3000, () => console.log(`App running on port ${3000}`));
